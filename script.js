cb0ed665381c1dd9dbf1d11d9d60fa806eb1537c
Samuel Silva <samuel7silva@outlook.com>
2022-06-02 16:47:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function loading() {
+    var img = document.getElementById("image");
+    var data = new Date();
+    var msge = document.getElementById("msg");
+    var hora = data.getHours();
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        msge.innerHTML = `Bom Dia !`;
+        document.body.style.background = "#5A7E96";
+        img.src = "assets/morning.png";
+    } else if (hora >= 12 && hora < 18) {
+        //boa tarde
+        msge.innerHTML = `Boa Tarde !`;
+        img.src = "assets/afternoon.png";
+        document.body.style.background = "#CF8752";
+    } else {
+        //boa noite
+        img.src = "assets/night.png";
+        document.body.style.background = "#221A26";
+    }
+}
+
+function showHour() {
+    var horas = document.getElementById("horas");
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s;
+    horas.innerHTML = time;
+
+    setTimeout(showHour, 1000);
+}
+
+showHour();
